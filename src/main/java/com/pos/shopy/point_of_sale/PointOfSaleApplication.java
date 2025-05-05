7e2b1d4a06ba6827fbe3e585b336985d579a5698
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-05-05 13:23:27 +0530

    add springdoc api

--- src/main/java/com/pos/shopy/point_of_sale/PointOfSaleApplication.java
+++ src/main/java/com/pos/shopy/point_of_sale/PointOfSaleApplication.java
@@ -2,10 +2,8 @@ package com.pos.shopy.point_of_sale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class PointOfSaleApplication {
 
 	public static void main(String[] args) {
